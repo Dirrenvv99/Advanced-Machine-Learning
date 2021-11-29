72c6f1c202c322672b06f0557997dda9c2591e9c
Oli4B <olivier.brahma@gmail.com>
2021-11-29 16:11:23 +0100

    Ik kom er niet helemaal uit

--- Week 2/HMC.py
+++ Week 2/HMC.py
@@ -4,54 +4,80 @@ import matplotlib.pyplot as plt
 
 
 def E(x):
-    return 0.5*(250.25*x[0]**2 - x[0]*x[1]*2*249.75 + 250.25*x[1])
+    # return 0.5*(250.25*x[0]**2 - x[0]*x[1]*2*249.75 + 250.25*x[1])
+    return 0.5*250.25*(x[0]**2+x[1]**2) + 249.75*x[0]*x[1]
 
 
 def gradE(x):
     return np.array([250.25* x[0] - 249.75*x[1], 250.25* x[1] - 249.75*x[0]])
 
 
-def update_step(x_old, tau, eps):
-    p = np.random.multivariate_normal(np.array([0,0]), np.eye(2), 1)[0] 
-    H_old = np.dot(p,p)/2 + E(x_old)
-    grad = gradE(x_old)
+def HMC(tau, eps, epochs):
+    np.random.seed(0)
+    samples = [np.random.multivariate_normal(np.array([0,0]), np.eye(2), 1)[0]]
+    rejections = 0
+    for _ in tqdm(range(epochs)):
+        x = np.copy(samples[-1])
+        g = gradE(x)
+        p = np.random.multivariate_normal(np.array([0,0]), np.eye(2), 1)[0]
+        
+        H_old = np.dot(p,p)/2 + E(x)
+        x_new = np.copy(x)
 
-    for _ in range(tau):
-        p = p - eps * grad/2
-        x_new  = x_old + eps * p
-        grad = gradE(x_new)
-        p = p - eps * grad/2
-    
-    Enew = E(x_new)
-    Hnew = np.dot(p,p)/2 + Enew
-    dH = Hnew - H_old
+        for _ in range(tau):    # Leapfrog
+            p -= eps * g/2
+            x_new += eps * p
+            g = gradE(x_new)
+            p -= eps * g/2
+        
+        H_new = np.dot(p,p)/2 + E(x_new)
+        dH = H_new - H_old
 
-    if dH < 0 or np.random.random() < np.exp(-1 * dH):
-        return x_new, 0
+        if dH != dH:
+            print("something went wrong and produces NaN values")
 
-    return x_old, 1
+        if dH < 0 or np.random.random() < np.exp(-dH):
+            samples.append(x_new)
+
+        else:
+            # samples.append(x)
+            rejections += 1
+
+    return samples, rejections
 
 
 def main():
-    iter = 10000
-    update_steps = 10
+    epochs = 10000
 
-    taus = [10, 30, 50]
-    epss = [i*0.001 for i in range(1,10,2)]
+    taus = [5, 10]
+    epss = [i*0.001 for i in range(10, 100, 30)] #.05, .1]
+    fig, axs = plt.subplots(len(epss), len(taus))
+
+    np.random.seed(0)
+    real_samples = np.random.multivariate_normal([0,0], [[250.25,-249.75],[-249.75,250.25]], 100)
 
     for tau, eps in [(i, j) for i in taus for j in epss]:
-        np.random.seed(0)
-        samples = []
-        rejections = 0
-        for _ in tqdm(range(iter)):
-            x = np.random.multivariate_normal(np.array([0,0]), np.eye(2), 1)[0]
-            for _ in range(update_steps):
-                x, rejected = update_step(x, tau, eps)
-                rejections += rejected
-            samples.append(x)
+        samples, rejections = HMC(tau, eps, epochs)
         
         print(f"{tau},\t{eps}\t{rejections}")
+        ei, ti = epss.index(eps), taus.index(tau)
+        # print(samples)
+
+        axs[ei, ti].scatter([x[0] for x in real_samples], [x[1] for x in real_samples], color = "blue", label = "normally sampled", marker = ".")
+        axs[ei, ti].scatter([x[0] for x in samples], [x[1] for x in samples], color = "red", label = "samples")
+        axs[ei, ti].set_title(f"sample plot with tau: {tau}; epsilon: {eps}")
+        if (ei+ti==0):
+            axs[ei, ti].legend()
+    # fig.tight_layout(pad = 3.0)
+    plt.show()
         
 
 if __name__ == '__main__':
     main()
+
+
+"""
+Optimal value Approx:
+Sigma          ~
+Rejection Rate ~
+"""
