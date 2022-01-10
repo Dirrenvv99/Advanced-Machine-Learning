0053dcb10fcede17d285e08d53facf9e0cf089be
Oli4B <olivier.brahma@gmail.com>
2022-01-10 15:18:34 +0100

    weights worden nu ook random aangemaakt

--- Week 5/main_template.py
+++ Week 5/main_template.py
@@ -35,24 +35,24 @@ def mf_approx(n, m_ex, w, th, smoothing=.7, eps=10**-13):
     return error_mf, iterations, m
 
 
-def bp(n, m_ex, w, th, eps=10**-13):
+def bp(n, m_ex, w, th, c, eps=10**-13):
     # Belief Propagation
     a = np.random.normal(size=(n,n))             # random init
 
     da = 1
     iterations = 0
-    while(da > eps):
+    while(da > eps and iterations < 1000):
         iterations += 1
         a_old = a
-        
-        m_pos = 2*np.cosh(w + th + np.sum(a, axis=1))   # axis =1 misschien niet goed
-        m_neg = 2*np.cosh(-w + th + np.sum(a, axis=1))
+
+        m_pos = 2*np.cosh(w + th + np.sum(np.multiply(a,c), axis=0))
+        m_neg = 2*np.cosh(-w + th + np.sum(np.multiply(a,c), axis=0))
 
         a = .5 * (np.log(m_pos) - np.log(m_neg))
 
         da = np.max(np.abs(a-a_old))
 
-    m = np.tanh(np.sum(a) + th)
+    m = np.tanh(np.sum(a, axis=0) + th)
     error_bp = np.sqrt(1/n*np.sum(m-m_ex)**2) # final error
 
     return error_bp, iterations, m
@@ -60,23 +60,6 @@ def bp(n, m_ex, w, th, eps=10**-13):
 
 def main():
     np.random.seed(37) #TODO random over weights
-    # toggle between full and sparse Ising network
-    if full:                    # full weight matrix
-        J0 = 0                  # J0 and J are as defined for the SK model
-        J = 0.5
-        w = J0/n+J/np.sqrt(n)*np.random.normal(size = (n,n))
-        np.fill_diagonal(w,0)
-        w = np.tril(w)+np.tril(w).T
-        c = ~(w==0)             # neighborhood graph fully connected
-
-    else:                       # sparse weight matrix
-        c1 = 0.5                # connectivity is the approximate fraction of non-zero links in the random graph on n spins
-        k = c1*n
-        beta = 0.5
-        w = sprandsym(n,c1)     # symmetric weight matrix w with c1*n^2 non-zero elements
-        np.fill_diagonal(w,0)
-        c = ~(w==0)             # sparse 0,1 neighborhood graph
-        w = beta*((w>0).astype(int)-(w<0).astype(int)) # w is sparse with +/-beta on the links
 
     x = np.linspace(0, 2, num=10)
     error_mean, error_std = [], []
@@ -86,7 +69,25 @@ def main():
     for Jth in tqdm(x):
         errors, iters, error_chis = [],[],[]
 
-        for _ in range(5):
+        for _ in range(10):
+
+            # toggle between full and sparse Ising network
+            if full:                    # full weight matrix
+                J0 = 0                  # J0 and J are as defined for the SK model
+                J = 0.5
+                w = J0/n+J/np.sqrt(n)*np.random.normal(size = (n,n))
+                np.fill_diagonal(w,0)
+                w = np.tril(w)+np.tril(w).T
+                c = ~(w==0)             # neighborhood graph fully connected
+
+            else:                       # sparse weight matrix
+                c1 = 0.5                # connectivity is the approximate fraction of non-zero links in the random graph on n spins
+                k = c1*n
+                beta = 0.5
+                w = sprandsym(n,c1)     # symmetric weight matrix w with c1*n^2 non-zero elements
+                np.fill_diagonal(w,0)
+                c = ~(w==0)             # sparse 0,1 neighborhood graph
+                w = beta*((w>0).astype(int)-(w<0).astype(int)) # w is sparse with +/-beta on the links
 
             # Jth=.1
             # np.random.seed(0)
@@ -103,15 +104,20 @@ def main():
             klad = np.outer(p_ex,np.ones(shape=(1,n)))*sa
             chi_ex = np.dot(sa.T,klad)-np.dot(m_ex,m_ex.T) # exact connected correlations
 
+            # print(1)
             error_mf, iter_mf, m_mf = mf_approx(n,m_ex,w,th, smoothing=.5)
-            chi_mf = np.linalg.inv(np.eye(n)/(1-m_mf**2)-w)
-            error_chi_mf = np.sqrt(1/n*np.sum(chi_ex-chi_mf)**2)
+            # print(2)
+
+            # chi_mf = np.linalg.inv(np.eye(n)/(1-m_mf**2)-w)
+            error_chi_mf = 0 #np.sqrt(1/n*np.sum(chi_ex-chi_mf)**2)
             # print(error_chi_mf)
             # print() # np.sqrt(np.mean(np.square(np.dot(sa.T,klad)-np.dot(m_ex,m_ex.T))))
             # print("MF APPROX")
             # print(f"ERROR: {error_mf},\tITER: {iter_mf},\tCHI: {chi_mf}\n")
 
-            error_bp, iter_bp, m_bp = bp(n,m_ex,w,th)
+            error_bp, iter_bp, m_bp = bp(n,m_ex,w,th, c)
+            # print(3)
+
             chi_bp = 0#np.dot(sa.T,klad)-np.dot(m_bp,m_bp.T) # exact connected correlations
             # print("BP")
             # print(f"ERROR: {error_bp},\tITER: {iter_bp},\tCHI: {chi_bp}\n")
