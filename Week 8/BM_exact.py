aed515b6048ffc7837570923d2bccd137b5cbb8c
Matthijs Neutelings <matthijsneutelings@hotmail.nl>
2022-03-26 11:38:43 +0100

    MH update

--- Week 8/BM_exact.py
+++ Week 8/BM_exact.py
@@ -29,11 +29,41 @@ def likelihood(w,theta, all_states, data):
     nom = [0.5*np.dot(s,np.dot(w,s)) + np.dot(theta, s) for s in data] 
     return np.mean(nom, axis=0) - np.log(Z)
 
+def a_value(w, theta, pattern, site):
+    diff = np.exp((2*pattern[site]-1)*np.dot(w[site], pattern) - (2*pattern[site]-1)*theta[site])
+    return diff
+
+
+def MH_sampler(w, theta, NrofFlips, iters):
+    singles = [] 
+    doubles = []
+    pattern = np.random.randint(0,2,NrofFlips)
+    for _ in range(iters):
+        flip = np.random.randint(0, NrofFlips)
+        a  = a_value(w,theta,pattern, flip)
+        if a > 1:
+            pattern[flip] = -(pattern[flip]-1)
+            singles.append(pattern)
+            w_spins = np.outer(pattern, pattern)
+            np.fill_diagonal(w_spins, 0)
+            doubles.append(w_spins)
+        elif np.random.random() < a:
+            pattern[flip] = -(pattern[flip]-1)
+            singles.append(pattern)
+            w_spins = np.outer(pattern, pattern)
+            np.fill_diagonal(w_spins, 0)
+            doubles.append(w_spins)
+    singles_mean = np.mean(singles, axis=0)
+    doubles_mean = np.mean(doubles, axis=0)
+    # print(singles_mean, doubles_mean)
+    return singles_mean, doubles_mean
+
 
 def clamped_statistics(data):
     single = 1/(len(data)) * np.sum(data, axis = 0)
     data_needed = np.array([np.outer(x,x) for x in data])
     double = 1/(len(data)) * np.sum(data_needed, axis = 0)
+    np.fill_diagonal(double, 0)
     return single, double
 
 
@@ -44,10 +74,11 @@ def free_statistics(w, theta, all_states, all_states_outer, w_zero = False):
         double = 3000
     else:
         double = np.sum([out * p for out, p in zip(all_states_outer, p_s)], axis = 0)
+        np.fill_diagonal(double, 0)
     return single, double
 
 
-def BM_exact(data, NrofSpins, NrofData, lr, threshold, w_zero = False):
+def BM_exact(data, NrofSpins, NrofData, lr, threshold, w_zero = False, MH_stats = False):
     #initialize w and theta randomly
     if w_zero:
         w = np.zeros((NrofSpins, NrofSpins))
@@ -56,17 +87,21 @@ def BM_exact(data, NrofSpins, NrofData, lr, threshold, w_zero = False):
         np.fill_diagonal(w,0.) 
     theta = np.random.randn(NrofSpins)
 
-    all_states = list(product(range(2), repeat = NrofSpins))
-    all_states = [np.array(s) for s in all_states]
-    all_states_outer = [np.outer(s,s) for s in all_states]
+    if (not MH_stats):
+        all_states = list(product(range(2), repeat = NrofSpins))
+        all_states = [np.array(s) for s in all_states]
+        all_states_outer = [np.outer(s,s) for s in all_states]
+        likelihood_chain = [likelihood(w, theta, all_states, data)]
+        gradient_chain = []
     print(np.mean(w))
-    likelihood_chain = [likelihood(w, theta, all_states, data)]
-    gradient_chain = []
     i = 0
     single_clamped, double_clamped = clamped_statistics(data)
     
     while 1:
-        single_free, double_free = free_statistics(w, theta, all_states, all_states_outer, w_zero)
+        if (not MH_stats):
+            single_free, double_free = free_statistics(w, theta, all_states, all_states_outer, w_zero)
+        else:
+            single_free, double_free = MH_sampler(w, theta, NrofSpins, 1000)
         gradient_double = double_clamped - double_free
         if w_zero:
             w = np.zeros((NrofSpins, NrofSpins))
@@ -76,8 +111,9 @@ def BM_exact(data, NrofSpins, NrofData, lr, threshold, w_zero = False):
         gradient_single = single_clamped - single_free
         theta += lr * gradient_single
 
-        likelihood_chain.append(likelihood(w,theta, all_states, data))
-        gradient_chain.append((np.mean(gradient_single), np.mean(gradient_double)))
+        if (not MH_stats):
+            likelihood_chain.append(likelihood(w,theta, all_states, data))
+            gradient_chain.append((np.mean(gradient_single), np.mean(gradient_double)))
         
         if w_zero:
            if np.allclose(single_clamped, single_free, rtol=0, atol = threshold * 1/lr):
